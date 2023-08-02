2200f60e66671dd5dbcc19792b54753108e29d84
hoang bao <baohoang.7882@hotmail.com>
2023-08-02 14:39:12 +0700

    feat: add user data access

---
+++ DataAccess/IUserInfoDAO.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess {
+    public interface IUserInfoDAO {
+        public UserInfo Get(int id);
+        public void Delete(int id);
+        public void Create(UserInfo user);
+        public void Update(UserInfo user);
+        public List<UserInfo> Get();
+
+        public UserInfo Login(string username, string password);
+    }
+}
---
+++ DataAccess/Implementation/UserInfoDAO.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Implementation {
+    public class UserInfoDAO : DataAccessBase<UserInfo>, IUserInfoDAO {
+        public UserInfoDAO(ProjectMessageContext context) : base(context) {
+        }
+
+        public void Delete(int id) {
+            var user = Get(id);
+            user.IsDeleted = (byte) Status.Deleted;
+            Update(user);
+        }
+
+        public UserInfo Get(int id) {
+            return GetAll().FirstOrDefault(p => p.Id == id && p.IsDeleted == (byte) Status.Available);
+        }
+
+        public List<UserInfo> Get() {
+            return GetAll().ToList();
+        }
+
+        public UserInfo Login(string username, string password) {
+            throw new NotImplementedException();
+        }
+
+        private UserInfo LoginWithUserName(string username, string password) {
+            var userInfo = GetAll().FirstOrDefault(p => p.Username.Equals(username, StringComparison.OrdinalIgnoreCase) && p.Password.Equals(password) && p.IsDeleted == (byte) Status.Available);
+            return userInfo != null ? userInfo : LoginWithEmail(username, password);
+        }
+
+        private UserInfo LoginWithEmail(string email, string password) {
+            return GetAll().FirstOrDefault(p => p.Email.Equals(email, StringComparison.OrdinalIgnoreCase) && p.IsDeleted == (byte) Status.Available);
+        }
+    }
+}
