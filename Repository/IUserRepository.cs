66194f7d0fa6200e7d3f628aa916a3de83f4a0e4
hoang bao <baohoang.7882@hotmail.com>
2023-08-18 15:53:27 +0700

    feat: add user repository

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository {
+    public interface IUserRepository {
+        public UserInfo Create(UserInfo userInfo);
+        public UserInfo Update(UserInfo userInfo);
+        public UserInfo Get(int id);
+        public void Delete(int id);
+        public List<UserInfo> Get();
+
+        public UserInfo Login(string email, string password);
+    }
+}
---
+++ Repository/Implementation/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementation {
+    public class UserRepository : IUserRepository {
+        private IUserInfoDAO _userInfoDAO;
+
+        public UserRepository(IUserInfoDAO userInfoDAO) {
+            _userInfoDAO = userInfoDAO;
+        }
+
+        public UserInfo Create(UserInfo userInfo) {
+            _userInfoDAO.Create(userInfo);
+            return _userInfoDAO.GetByEmail(userInfo.Email);
+        }
+
+        public void Delete(int id) {
+            var db = _userInfoDAO.Get(id);
+            db.IsDeleted = (byte) Status.Deleted;
+            _userInfoDAO.Update(db);
+        }
+
+        public UserInfo Get(int id) {
+            return _userInfoDAO.Get(id);
+        }
+
+        public List<UserInfo> Get() {
+            return _userInfoDAO.Get();
+        }
+
+        public UserInfo Login(string email, string password) {
+            throw new NotImplementedException();
+        }
+
+        public UserInfo Update(UserInfo userInfo) {
+            _userInfoDAO.Update(userInfo);
+            return _userInfoDAO.GetByEmail(userInfo.Email);
+        }
+    }
+}
