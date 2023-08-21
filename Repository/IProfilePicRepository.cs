ae75885f424acace104fc53db88d30dcc546cea9
hoang bao <baohoang.7882@hotmail.com>
2023-08-21 15:32:31 +0700

    feat: add repository for profile pic

---
+++ Repository/IProfilePicRepository.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository {
+    public interface IProfilePicRepository {
+        public ProfilePic GetByProfilePicId(int id);
+        public List<ProfilePic> GetByUserId(int id);
+        public void Delete(int id);
+        public ProfilePic Create(ProfilePic pic);
+        public ProfilePic Update(ProfilePic pic);
+        public List<ProfilePic> Get();
+    }
+}
---
+++ Repository/Implementation/ProfilePicRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccess;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Implementation {
+    public class ProfilePicRepository : IProfilePicRepository{
+        private IProfilePicDAO _profilePicDAO;
+
+        public ProfilePicRepository(IProfilePicDAO profilePicDAO) {
+            _profilePicDAO = profilePicDAO;
+        }
+
+        public ProfilePic Create(ProfilePic pic) {
+            Create(pic);
+            return _profilePicDAO.GetByPath(pic.ImagePath);
+        }
+
+        public void Delete(int id) {
+            _profilePicDAO.Delete(id);
+        }
+
+        public List<ProfilePic> Get() {
+            return _profilePicDAO.Get();
+        }
+
+        public ProfilePic GetByProfilePicId(int id) {
+            return _profilePicDAO.GetByProfilePicId(id);
+        }
+
+        public List<ProfilePic> GetByUserId(int id) {
+            return _profilePicDAO.GetByUserId(id);
+        }
+
+        public ProfilePic Update(ProfilePic pic) {
+            _profilePicDAO.Update(pic);
+            return _profilePicDAO.GetByPath(pic.ImagePath);
+        }
+    }
+}
