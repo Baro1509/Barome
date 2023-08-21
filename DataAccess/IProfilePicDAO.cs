c6382c4984f019766f44a80b12c3c48c56fbd507
hoang bao <baohoang.7882@hotmail.com>
2023-08-21 15:32:17 +0700

    feat: add data access for profile pic

---
+++ DataAccess/IProfilePicDAO.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess {
+    public interface IProfilePicDAO {
+        public ProfilePic GetByProfilePicId(int id);
+        public List<ProfilePic> GetByUserId(int id);
+        public ProfilePic GetByPath(string path);
+        public void Delete(int id);
+        public void Create(ProfilePic pic);
+        public void Update(ProfilePic pic);
+        public List<ProfilePic> Get();
+    }
+}
---
+++ DataAccess/Implementation/ProfilePicDAO.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Implementation {
+    public class ProfilePicDAO : DataAccessBase<ProfilePic>, IProfilePicDAO {
+        public ProfilePicDAO(ProjectMessageContext context) : base(context) {
+        }
+
+        public void Delete(int id) {
+            var db = GetByProfilePicId(id);
+            db.IsDeleted = (byte)Status.Deleted;
+            Update(db);
+        }
+
+        public List<ProfilePic> Get() {
+            return GetAll().ToList();
+        }
+
+        public ProfilePic GetByPath(string path) {
+            return GetAll().FirstOrDefault(p => p.ImagePath.Equals(path));
+        }
+
+        public ProfilePic GetByProfilePicId(int id) {
+            return GetAll().FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<ProfilePic> GetByUserId(int id) {
+            return GetAll().Where(p => p.UserId == id).ToList();
+        }
+    }
+}
